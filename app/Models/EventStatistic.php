6b50a1b5751768c9c5d793eae3be58ef82d86e4c
pramana yuda <pramanayuda772@gmail.com>
2026-03-16 14:56:42 +0700

    push statistik dashboard

--- app/Models/EventStatistic.php
+++ app/Models/EventStatistic.php
@@ -16,4 +16,15 @@ class EventStatistic extends Model
         'not_yet_registered',
         'calculated_at'
     ];
+
+    protected $casts = [
+        'event_id' => 'integer',
+        'total_participants' => 'integer',
+        'paid_participants' => 'integer',
+        'unpaid_participants' => 'integer',
+        'total_registrations' => 'integer',
+        'already_registered' => 'integer',
+        'not_yet_registered' => 'integer',
+        'calculated_at' => 'datetime',
+    ];
 }
